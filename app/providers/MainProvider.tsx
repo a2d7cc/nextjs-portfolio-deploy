81ae61eed5b2f4df61619999c27336ca924a4278
a2d7cc <a2d7cc@gmail.com>
2023-04-05 15:21:08 +0200

    React-query configured

--- app/providers/MainProvider.tsx
+++ app/providers/MainProvider.tsx
@@ -2,12 +2,23 @@ import { store } from "@/store/store";
 import {FC, PropsWithChildren} from "react"
 import { Provider } from "react-redux";
 import ReduxToast from "./ReduxToast";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		},
+	},
+})
 
 const MainProvider: FC<PropsWithChildren> = ({children}) => {
   return (
     <Provider store={store}>
-      <ReduxToast />
-      {children}
+      <QueryClientProvider client={queryClient}>
+        <ReduxToast />
+        {children}
+      </QueryClientProvider>
     </Provider>
   )
 };
