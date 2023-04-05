0275f8ce9519d86e7c09753f5b8297a01cf98c57
a2d7cc <a2d7cc@gmail.com>
2023-04-05 15:27:41 +0200

    HeadProvider, Favicons

---
+++ app/providers/HeadProvider/HeadProvider.tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import NextNProgress from 'nextjs-progressbar'
+import { FC, PropsWithChildren } from 'react'
+import Favicons from './Favicons'
+import { accentColor } from '@/configs/constants'
+
+const HeadProvider: FC<PropsWithChildren> = ({ children }) => {
+	return (
+		<>
+			<NextNProgress
+				color={accentColor}
+				startPosition={0.3}
+				stopDelayMs={200}
+				height={3}
+				showOnShallow={true}
+			/>
+			<Head>
+				<meta charSet="UTF-8" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, maximum-scale=5"
+				/>
+
+				<Favicons />
+
+				<meta name="theme-color" content={'#181B1E'} />
+				<meta name="msapplication-navbutton-color" content={'#181B1E'} />
+				<meta
+					name="apple-mobile-web-app-status-bar-style"
+					content={'#181B1E'}
+				/>
+				<link rel="manifest" href="/manifest.json" />
+			</Head>
+			{children}
+		</>
+	)
+}
+
+export default HeadProvider
